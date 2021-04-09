80dc6f0c3dec92dcc8a74b849b3abd84d4471b81
Krishna Manchikanti <krishnamanchikanti@Krishnas-MacBook-Air.local>
2021-04-09 15:30:51 +0530

    Server is up at port 8000

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.get('/',(req,res)=>{
+    return res.send('hello');
+})
+
 app.listen(port,(err)=>{
     if(err){console.log(`Error in starting server ${err}`)};
     console.log(`Server is up at port ${port}`);

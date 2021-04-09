065d814a2fb1536b27d41618b2667f6a42ebf1ef
Krishna Manchikanti <krishnamanchikanti@Krishnas-MacBook-Air.local>
2021-04-09 17:14:14 +0530

    setting up ejs and sass

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const path = require('path');
+const sassMiddleware = require('node-sass-middleware');
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'./views'));
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css', 
+    debug: true, 
+    outputStyle: 'compressed', 
+    prefix:  '/css'
+}));
 
 app.get('/',(req,res)=>{
-    return res.send('hello');
-})
+    return res.render('home.ejs');
+});
 
 app.listen(port,(err)=>{
     if(err){console.log(`Error in starting server ${err}`)};
